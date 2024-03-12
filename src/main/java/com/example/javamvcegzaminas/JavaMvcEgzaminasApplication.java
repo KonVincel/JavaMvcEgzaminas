9a882874cc5060b2a72c7c318c2c2a3a2510b54f
DESKTOP-6IF9S8G\Vincel <doctorvincel@gmail.com>
2024-03-12 09:22:49 +0200

    init

---
+++ src/main/java/com/example/javamvcegzaminas/JavaMvcEgzaminasApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamvcegzaminas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvcEgzaminasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMvcEgzaminasApplication.class, args);
+    }
+
+}
